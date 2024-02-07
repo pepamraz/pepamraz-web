722b7e129a56ba3458e64dc023dbcb5822e9fdf7
Pepa Mráz <pepamraz1@gmail.com>
2024-02-07 23:23:35 +0100

    edit base for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pepamraz-web/',
+  base: '/',
   plugins: [react()],
   server: {
     watch: {
