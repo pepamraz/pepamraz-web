0d56252672b8956b3fb100716aad0b200155ce59
Pepa Mráz <pepamraz1@gmail.com>
2024-02-07 22:52:35 +0100

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pepamraz.cz/',
+  base: '/pepamraz-web/',
   plugins: [react()],
   server: {
     watch: {
