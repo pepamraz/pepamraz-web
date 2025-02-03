801145ff8fa19cf9ca19304157dd0285ae16cae9
Pepa Mráz <pepamraz1@gmail.com>
2025-02-03 03:40:59 +0100

    add footer projects section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -39,26 +39,41 @@ const Footer: React.FC = () => {
 
   const nextStepsLinks = [
     {
-      name: "Kup si něco hezkého na Greenly.cz",
+      name: "🍃 Kup si něco na Greenly.cz",
       url: "https://greenly.cz",
     },
     {
-      name: "Koukni na moje fotky",
+      name: "📸 Koukni na moje fotky",
       url: "https://instagram.com/pepa_mraz",
     },
     {
-      name: "Zhlédni má videa",
+      name: "🎥 Zhlédni má videa",
       url: "https://www.youtube.com/channel/UCMlo7Ew_x0FjDqlvctXkGTg",
     },
     {
-      name: "Napiš mi email",
-      url: "mailto:boss@pepamraz.cz",
+      name: "✉️ Napiš mi e-mail",
+      url: "mailto:ahoj@pepamraz.cz",
     },
+  ];
+
+  const allProjectsLinks = [
+    {
+      emoji: "🌿",
+      name: "Greenly.cz",
+      description: "E-shop",
+      url: "https://greenly.cz",
+    },
+    {
+      emoji: "🖼️",
+      name: "img.pepamraz.cz",
+      description: "Komprese obrázků pro web",
+      url: "https://img.pepamraz.cz",
+    }
   ];
 
   return (
     <div className="bg-primary border-secondary border-t-8 p-12">
-      <div className="grid grid-cols-1 md:grid-cols-3 max-w-5xl m-auto gap-y-6">
+      <div className="grid grid-cols-1 md:grid-cols-4 max-w-5xl m-auto gap-y-6">
         {/* Social Links Section */}
         <div className="one flex flex-col gap-y-4">
           <h2 className="text-4xl text-secondary font-bold">Pepa Mráz</h2>
@@ -103,6 +118,33 @@ const Footer: React.FC = () => {
                   className="no-underline text-white hover:text-secondary"
                 >
                   {link.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* All of my projects */}
+        <div className="four">
+          <h2 className="text-2xl text-secondary font-bold">Všechny projekty</h2>
+          <ul className="footer-links text-white">
+            {allProjectsLinks.map((link, index) => (
+              <li key={index}>
+                <a
+                  href={link.url}
+                  target={link.url.startsWith("http") ? "_blank" : "_self"}
+                  rel="noopener noreferrer"
+                  className="no-underline text-white hover:text-secondary flex"
+                >
+                  <div className="emoji">
+                    {link.emoji}
+                  </div>
+                  <div className="project">
+                    {link.name}
+                    <small>
+                      &nbsp;-&nbsp;{link.description}
+                    </small>
+                  </div>
                 </a>
               </li>
             ))}
--- src/components/Signature.tsx
+++ src/components/Signature.tsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 const Signature: React.FC = () => {
+  const year = useMemo(() => new Date().getFullYear(), []);
+
   return (
-    <div className="flex justify-center bg-secondary text-white text-stroke font-black">
-      &copy; mistrovské dílo budoucnosti 2024
+    <div className="flex justify-center bg-secondary text-white font-black text-shadow-lg py-2">
+      &copy; Pepa Mráz {year}
     </div>
   );
 };
